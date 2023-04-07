ed84d9114b4ab17c5e0a5dc8a412c0d57743a402
Masamitsu MURASE <masamitsu.murase@gmail.com>
2023-04-08 00:10:39 +0900

    Version 1.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='deform_conv2d_onnx_exporter',
-    version='1.1.0',
+    version='1.2.0',
     description='A library to support onnx export of deform_conv2d in PyTorch.',
     long_description=long_description,
     long_description_content_type='text/markdown',
